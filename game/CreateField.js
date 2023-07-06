fbc68c5705e91311e3bcbc57ee954479c94ee825
78nick <138786569+78nick@users.noreply.github.com>
2023-07-06 17:50:48 +0300

    Add files via upload

--- game/CreateField.js
+++ game/CreateField.js
@@ -1,3 +1,34 @@
+import { createMenu } from "./CreatMenu.js";
 export const createField = (lvl) => {
+   const main = document.querySelector("main");
+   main.classList.add("null");
+
+
+
    console.log(lvl);
+   let lvlText;
+
+   switch (lvl) {
+      case 8:
+         lvlText = "Лёгкий уровень";
+         break;
+      case 12:
+         lvlText = "Средний уровень";
+         break;
+      case 16:
+         lvlText = "Сложный уровень";
+         break;
+   }
+
+   const game = document.querySelector(".game");
+   game.style.display = "block";
+
+   const level = game.querySelector(".lvl");
+   level.textContent = lvlText;
+
+
+   const menu = game.querySelector(".btn");
+   menu.addEventListener('click', () => {
+      createMenu();
+   })
 }
